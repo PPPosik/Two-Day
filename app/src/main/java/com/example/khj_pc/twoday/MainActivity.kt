121c1cf4b202a2000a440d74b58caf3e79aad0c5
정승욱 <wjdtmddnr24@naver.com>
2018-09-04 20:55:39 +0900

    texactivity 생성

--- app/src/main/java/com/example/khj_pc/twoday/MainActivity.kt
+++ app/src/main/java/com/example/khj_pc/twoday/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.khj_pc.twoday
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.support.design.widget.Snackbar
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +15,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         editText.customSelectionActionModeCallback = CustomCallback(this, editText)
+
+        val fab = findViewById<View>(R.id.fab) as FloatingActionButton
+        fab.setOnClickListener { view ->
+            startActivity(Intent(this, TexActivity::class.java))
+        }
     }
 }
 
